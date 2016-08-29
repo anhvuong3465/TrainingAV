45d5eee20f4184896f5bb94ee12ce69dcdf052c5
anhvuong3465 <anhvuongtran95@gmail.com>
2016-08-29 09:07:22 +0000

    Update 2-sophuc.cpp

--- 2-sophuc.cpp
+++ 2-sophuc.cpp
@@ -1,17 +1,15 @@
-#include<iostream>
-#include<stdlib.h>
-#include<math.h>
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
 
 
 using namespace std;
 
-class sophuc
-	{
+class sophuc {
 	private:
 		float pt, pa;
 	public:
-		sophuc()
-		{
+		sophuc() {
 			pt=0;
 			pa=0;
 		}
@@ -25,66 +23,60 @@ class sophuc
 
 void sophuc::nhap ()
 {
-	cout<<"Moi nhap phan thuc :";
-	cin>>pt;
-	cout<<"Moi nhap phan ao :";
-	cin>>pa;
+	cout << "Moi nhap phan thuc :";
+	cin >> pt;
+	cout << "Moi nhap phan ao :";
+	cin >> pa;
 }
 	
-void sophuc::xuat()
-{
-	if(pa<0)
-	cout<<pt<<pa<<"*i"<<"\n";
+void sophuc::xuat() {
+	if(pa < 0)
+	cout << pt << pa << "*i" << "\n";
 	else
-	cout<<pt<<"+"<<pa<<"*i"<<"\n";
+	cout << pt << "+" << pa << "*i" << "\n";
 		
 }
-void sophuc::cong(sophuc&c1,sophuc&c2)
-{
-	this ->pt=c1.pt+c2.pt;
-	this ->pa=c1.pa+c2.pa;
+void sophuc::cong(sophuc&c1, sophuc&c2) {
+	this -> pt = c1.pt+c2.pt;
+	this -> pa = c1.pa+c2.pa;
 }
-void sophuc::tru(sophuc&c1,sophuc&c2)
-{
-	this ->pt=c1.pt-c2.pt;
-	this ->pa=c1.pa-c2.pa;
+void sophuc::tru(sophuc&c1,sophuc&c2) {
+	this -> pt = c1.pt-c2.pt;
+	this -> pa = c1.pa-c2.pa;
 }
-void sophuc::nhan(sophuc&c1,sophuc&c2)
-{
-	this ->pt=c1.pt*c2.pt-c1.pa*c2.pa;
-	this ->pa=c1.pt*c2.pa+c1.pa*c2.pt;
+void sophuc::nhan(sophuc&c1,sophuc&c2) {
+	this -> pt = c1.pt*c2.pt-c1.pa*c2.pa;
+	this -> pa = c1.pt*c2.pa+c1.pa*c2.pt;
 }
-void sophuc::chia(sophuc&c1,sophuc&c2)
-{
+void sophuc::chia(sophuc&c1,sophuc&c2) {
 	sophuc kq;
-	if((c2.pt==0)&&(c2.pa==0)) return;
-	kq.pt=c2.pt/(pow(c2.pt,2)+pow(c2.pa,2));
-	kq.pa=-c2.pa/(pow(c2.pt,2)+pow(c2.pa,2));
+	if((c2.pt == 0)&&(c2.pa == 0)) return;
+	kq.pt = c2.pt/(pow(c2.pt,2) + pow(c2.pa,2));
+	kq.pa = -c2.pa/(pow(c2.pt,2)+pow(c2.pa,2));
 	(*this).nhan(c1,kq);
 }
 
-int main()
-{
+int main() {
 	sophuc c1,c2;
 	sophuc kq;
-	cout<<"Moi nhap vao hai so phuc"<<"\n";
-	cout<<"So thu nhat"<<"\n";
+	cout << "Moi nhap vao hai so phuc" << "\n";
+	cout << "So thu nhat" << "\n";
 	c1.nhap();
-	cout<<"So thu hai"<<"\n";
+	cout << "So thu hai" << "\n";
 	c2.nhap();
-	cout<<"So thu nhat là:";c1.xuat();
-	cout<<"So thu hai la:";c2.xuat();
-	cout<<"\n Ket qua cac phep toan:\n";
-	cout<<"\n Phep cong hai so:";
+	cout << "So thu nhat là:"; c1.xuat();
+	cout << "So thu hai la:";c2.xuat();
+	cout << "\n Ket qua cac phep toan:\n";
+	cout << "\n Phep cong hai so:";
 	kq.cong(c1,c2);
 	kq.xuat();
-	cout<<"\n Ket qua tru so thu nhat cho so thu hai la:";
+	cout << "\n Ket qua tru so thu nhat cho so thu hai la:";
 	kq.tru(c1,c2);
 	kq.xuat();
-	cout<<"\n Phep nhan hai so:";
+	cout << "\n Phep nhan hai so:";
 	kq.nhan(c1,c2);
 	kq.xuat();
-	cout<<"\n Ket qua chia so thu nhat cho so thu hai la:";
+	cout << "\n Ket qua chia so thu nhat cho so thu hai la:";
 	kq.chia(c1,c2);
 	kq.xuat();
 }
